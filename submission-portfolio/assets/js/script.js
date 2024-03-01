93bf3b176b9384911a9aad3dfbd71f5bd9b770cd
Azzaxy <abdurrohmanazis@gmail.com>
2024-03-01 13:09:04 +0700

    feat: add footer

---
+++ submission-portfolio/assets/js/script.js
@@ -0,0 +1,4 @@
+// Footer
+const footer = document.querySelector("footer p");
+const currentDate = new Date().getFullYear();
+footer.innerHTML = `<p>&#169; ${currentDate}, Made with 🔥🌾 by Abdurrohman Azis</p>`;
